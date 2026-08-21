8f4141e092e7a335c63bfce1c521867dcf09b68f
Pratham Jain <175920390+TeamDDPratham@users.noreply.github.com>
2026-08-22 03:59:27 +0530

    feat: add originkit global click effects animation

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import MouseEffects from "@/components/originkit/ui/clickeffects";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MouseEffects color="#06b6d4" interactionMode="sniper" effectSize={60} />
+      <Component {...pageProps} />
+    </>
+  );
 }
